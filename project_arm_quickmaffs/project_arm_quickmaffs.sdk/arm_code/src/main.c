f3051ca63026a437920a3ab832ffc209c2231d97
varouzan <varouzankn@gmail.com>
2018-12-10 17:38:42 -0500

    axi interface

---
+++ project_arm_quickmaffs/project_arm_quickmaffs.sdk/arm_code/src/main.c
@@ -0,0 +1,18 @@
+/*
+ * main.c
+ *
+ *  Created on: Dec 10, 2018
+ *      Author: varou
+ */
+#include "quickmaffs.h"
+#include "xparameters.h"
+#include "xil_io.h"
+#include "sleep.h"
+#include "xil_printf.h"
+#include <stdio.h>
+
+unsigned char raw[512]="A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lo";
+
+int main(){
+
+}
